612c60c20ceb06ffba6c42855eb5d3089fb390e8
swaroop-2007 <73997421+swaroop-2007@users.noreply.github.com>
2021-03-20 01:29:32 +0530

    Init

---
+++ objectDetection.py
@@ -0,0 +1,43 @@
+# Importing the libraries
+import torch
+from torch.autograd import Variable
+import cv2
+from data import BaseTransform, VOC_CLASSES as labelmap
+from ssd import build_ssd
+import imageio
+
+# Defining a function fpr the detections
+def detect(frame, net, transform):
+    height, width = frame.shape[:2]
+    frame_t = transform(frame)[0]
+    x = torch.from_numpy(frame_t).permute(2, 0, 1)
+    x = x.unsqueeze(0)
+    with torch.no_grad():
+        y = net(x)
+    detections = y.data
+    scale = torch.Tensor([width, height, width, height])
+    # detections = [batch, number of classes, number of occurence, (score, x0, Y0, x1, y1)]
+    for i in range(detections.size(1)):
+        j = 0
+        while detections[0, i, j, 0] >= 0.3:
+            pt = (detections[0, i, j, 1:] * scale).numpy()
+            cv2.rectangle(frame, (int(pt[0]), int(pt[1])), (int(pt[2]), int(pt[3])), (255, 0, 0), 2)
+            cv2.putText(frame, labelmap[i - 1], (int(pt[0]), int(pt[1])), cv2.FONT_HERSHEY_SIMPLEX, 2, (255, 255, 255), 2, cv2.LINE_AA)
+            j += 1
+    return frame
+# Creating the SSD neural network
+net = build_ssd('test') # We create an object that is our neural network ssd.
+net.load_state_dict(torch.load('ssd300_mAP_77.43_v2.pth', map_location = lambda storage, loc: storage)) # We get the weights of the neural network from another one that is pretrained (ssd300_mAP_77.43_v2.pth).
+
+# Creating the transformation
+transform = BaseTransform(net.size, (104/256.0, 117/256.0, 123/256.0)) # We create an object of the BaseTransform class, a class that will do the required transformations so that the image can be the input of the neural network.
+
+# Doing some Object Detection on a video
+reader = imageio.get_reader('test.mp4') #Opening the video.
+fps = reader.get_meta_data()['fps'] #Getting the fps frequence (frames per second).
+writer = imageio.get_writer('output.mp4', fps = fps) #Creating an output video with this same fps frequence.
+for i, frame in enumerate(reader): 
+    frame = detect(frame, net.eval(), transform) #Calling our detect function (defined above) to detect the object on the frame.
+    writer.append_data(frame) #Adding the frames 
+    print(i) #Print the number of the processed frames.
+writer.close() #Close the process that handles the creation of the output video.
